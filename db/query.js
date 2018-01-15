da9ddaf120a15616a154f864984dba3b8faa1108
Weston Gibler <westongibler@gmail.com>
2018-01-15 06:44:42 -0700

    finished routes and queries

--- db/query.js
+++ db/query.js
@@ -1,23 +1,33 @@
+
 var db = require('./connection');
 
 function getAll() {
-	// Get all cartoons and return them to the route
+	return db.select('*')
+	.from('cartoon');
 }
 
 function getOne(id) {
-	// Get one cartoon by the id and return it
+	return db.select('*')
+	.from('cartoon')
+	.where('id',id);
 }
 
 function create(cartoon) {
-	// Add a cartoon to the database
+	return db('cartoon')
+        .insert(body)
+        .returning('id')
 }
 
 function update(id, cartoon) {
-	// Update a cartoon with the specified id
+	return db.table('cartoon')
+	.update(book)
+	.where('id', id);
 }
 
 function remove(id) {
-	// Delete the specified cartoon
+	return db.del()
+	.from('cartoon')
+	.where('id', id)
 }
 
 module.exports = {
--- routes/cartoons.js
+++ routes/cartoons.js
@@ -1,20 +1,50 @@
-var express = require('express');
-var router = express.Router();
-// Bring in the queries object that has all of the db query functions
+const express = require('express');
+const router = express.Router();
+const db = require('../db/query');
+
 
 // Create the route for get all
-
+router.get('/', (req, res, next) => {
+  db.getAll()
+  .list()
+  .then((cartoons) => {
+    res.json(cartoons);
+});
+})
 
 // Create the route for get one
-
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+    db.getOne(id)
+      .then((cartoon) => {
+        res.json(cartoon)
+    })
+});
 
 // Create the route for creating an item
+router.post('/', (req, res, next) => {
+  db.create(req.body)
+  .then((id) => {
+    res.json({cartoon_id: id[0]});
+  })
+});
 
 
 // Create the route for updating
+router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+    db.update(id, req.body)
+      .then(() => res.send("updated!"))
 
+});
 
 // Route for deleting an item
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id;
+    db.remove(id)
+      .then(() => res.send("Deleted")
+    )
+});
 
 
 module.exports = router;
